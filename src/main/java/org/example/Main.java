54fc91171d4bbed36762a6c83fb65b026d65d050
oznurtopcu <topcuoznur121@gmail.com>
2025-03-11 00:28:14 +0300

    task-2 player finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Player player = new Player("aslı", 100, Weapon.AXE);
+        System.out.println("health reamining: " + player.healthRemaining());
+
+        Player player2 = new Player("aslı", 100, Weapon.AXE);
+        System.out.println("health reamining: " + player2.healthRemaining());
+
+        player.loseHealth((int) (player2.getWeapon().getDamage() * player2.getWeapon().getAttackSpeed()));
+        System.out.println("health reamining after damage: " + player.healthRemaining());
+
+        player.restoreHealth(70);
+        System.out.println("health reamining after restore: " + player.healthRemaining());
     }
 }
